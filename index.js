d07153019dd8a1a38a47c780dd7a9df309e1e57c
abc <abc@example.com>
2023-03-17 23:50:49 -0400

    made outline for js

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
+const inquirer = require('inquirer');
+
+// Define classes for different team members
+class Employee { /* ... */ }
+class Manager extends Employee { /* ... */ }
+class Engineer extends Employee { /* ... */ }
+class Intern extends Employee { /* ... */ }
+
+// Prompt user for team manager's information
+async function promptManager() { /* ... */ }
+
+// Prompt user for engineer's information
+async function promptEngineer() { /* ... */ }
+
+// Prompt user for intern's information
+async function promptIntern() { /* ... */ }
+
+// Prompt user to add another team member or finish
+async function promptAddTeamMember() { /* ... */ }
+
+// Generate HTML markup using provided data
+function generateHTML(data) { /* ... */ }
+
+// Start application
+async function start() { /* ... */ }
+
+start();
